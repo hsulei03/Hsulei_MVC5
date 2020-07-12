767b38e61e682629e3b1d6420ad24e03f25e7492
HsuLei <hsulei03@gmail.com>
2020-07-12 13:28:20 +0800

    Scaffolding, Layout佈檔 (2-6)~(2-7)

---
+++ MVC5_Exercise/MVC5_Exercise/Controllers/FriendsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC5_Exercise.Data;
+using MVC5_Exercise.Models;
+
+namespace MVC5_Exercise.Controllers
+{
+    public class FriendsController : Controller
+    {
+        private MVC5_ExerciseContext db = new MVC5_ExerciseContext();
+
+        // GET: Friends
+        public ActionResult Index()
+        {
+            return View(db.Friends.ToList());
+        }
+
+        // GET: Friends/Details/5
+        /// <summary>
+        /// 資料明細，需傳入id參數
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // GET: Friends/Create
+        /// <summary>
+        /// 新資資料-顯示用
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friends/Create
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]//Create, Edit, Delete 異動資料需搭配post方法
+        [ValidateAntiForgeryToken]//防止跨網站偽造請求的攻擊
+        public ActionResult Create([Bind(Include = "ID,Name,Phone,Email,City")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Friends.Add(friend);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(friend);
+        }
+
+        // GET: Friends/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: Friends/Edit/5
+        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
+        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Phone,Email,City")] Friend friend)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(friend).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(friend);
+        }
+
+        // GET: Friends/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Friend friend = db.Friends.Find(id);
+            if (friend == null)
+            {
+                return HttpNotFound();
+            }
+            return View(friend);
+        }
+
+        // POST: Friends/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Friend friend = db.Friends.Find(id);
+            db.Friends.Remove(friend);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVC5_Exercise/MVC5_Exercise/Models/Friend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5_Exercise.Models
+{
+    public class Friend
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string City { get; set; }
+
+    }
+}
